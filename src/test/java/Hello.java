ef921951f8a9a0fb82b42586734ca1e831b27736
AidanaBo <aydana.boronbaeva@gmail.com>
2021-09-20 10:26:59 +0300

    Update added more hello feature

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing Hello feature");
+        System.out.println("Doing more work");
     }
 }
